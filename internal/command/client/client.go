434973c22d61559512fac7d801e4bf1b0d29168a
takumin <takumiiinn@gmail.com>
2022-06-24 11:48:08 +0900

    feat(client): initial implemented

---
+++ internal/command/client/client.go
@@ -0,0 +1,33 @@
+package client
+
+import (
+	"github.com/urfave/cli/v2"
+
+	"github.com/takumin/gldap/internal/config"
+)
+
+func NewCommands(c *config.Config, f []cli.Flag) *cli.Command {
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:        "endpoint-url",
+			Aliases:     []string{"endpoint"},
+			Usage:       "endpoint url",
+			EnvVars:     []string{"ENDPOINT_URL", "ENDPOINT"},
+			Value:       c.Client.Endpoint,
+			Destination: &c.Client.Endpoint,
+		},
+	}
+	return &cli.Command{
+		Name:    "client",
+		Aliases: []string{"c", "cli"},
+		Usage:   "ldap client",
+		Flags:   append(flags, f...),
+		Action:  action(c),
+	}
+}
+
+func action(c *config.Config) func(ctx *cli.Context) error {
+	return func(ctx *cli.Context) error {
+		return nil
+	}
+}
